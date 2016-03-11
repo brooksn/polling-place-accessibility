d0cf1cc6df1eb4487afcf4ef2fd38fdd207cdb12
Nate Cornell <natec@rb-com.com>
2016-03-11 08:52:57 -0800

    Cleaned up json output; now valid JSON!! :)

--- csvfile.js
+++ csvfile.js
@@ -3,10 +3,14 @@ var parse = require('csv-parse');
 var list;
 fs.readFile('./src/data/VREData.csv', 'utf-8', (err, output) => {
   if (err) throw err;
-  parse(output, {columns: true}, (err, response) => {
+  parse(output, {columns: true, relax: false}, (err, response) => {
     list = response;
-    console.log(list);
-    fs.writeFile(`./vredataset.json`, list, (err) => {
+    fs.writeFile(`./src/data/vredataset.json`, JSON.stringify(list, null, 2)
+                 .replace(/\"{\\\"Categories\\\":/g, '{"Categories":')
+                 .replace(/\}\]\}\]\}\]\}\"/g, '}]}]}]}')
+                 .replace(/\\\"/g, '"')
+                 .replace(/\\\\\"/g, '\\\"')
+    , (err) => {
       if (err) throw err;
       console.log(`complete`);
     })
